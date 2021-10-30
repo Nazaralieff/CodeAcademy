ecfb46dc40e1a675b78792bb8b338ab8db15f559
Nazaralieff <nazaralyeff@gmail.comm>
2021-10-30 17:38:41 +0400

    Tasks

---
+++ C# Tasks/Task 4/VolumeOfBox/VolumeOfBox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VolumeOfBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Volume vol = new Volume()
+            {
+                width = 2,
+                lenhtg = 3,
+                height = 4
+            };
+            static void VolumeOfBox(Volume obj)
+            {
+                Console.WriteLine(obj.width * obj.height * obj.lenhtg);
+            }
+
+            VolumeOfBox(vol);
+        }
+
+    }
+        class Volume
+    {
+        public int width;
+        public int lenhtg;
+        public int height;
+    }
+}
